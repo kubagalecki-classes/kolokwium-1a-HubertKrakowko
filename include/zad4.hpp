39c3d4081b98192d56059746a011fa16a48a4253
HubertKrakowko <01123920@pw.edu.pl>
2021-01-14 12:05:18 +0000

    zad1,zad4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,3 +3,20 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
+double obliczMake(std::vector<Tagliatelle> vec) {
+	double M=0;
+	int c = vec.size()+1;
+	for (auto n : vec) {
+		M = M + (n.ileMaki(c));
+		c--;
+	}
+	if (M > 100) {
+		throw(10);
+	}
+	else if (M > 50) {
+		throw(0.5);
+	}
+	else {
+		return M;
+	}
+}
